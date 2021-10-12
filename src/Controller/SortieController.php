6fe16d1ba7de1806a103d1623d1270455a738f6b
Bastien <dsbastien7775@gmail.com>
2021-10-12 10:31:16 +0200

    Commit role hierarchie et role user

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -12,9 +12,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class SortieController extends AbstractController
 {
     /**
-     * @Route("/", name="liste")
+     * @Route("/", name="")
      */
     public function accueil(SortieRepository $sortieRepository): Response
+    {
+        $sorties = $sortieRepository->affichageSortieAccueil();
+        return $this->render('base.html.twig', [
+            'sorties' => $sorties,
+        ]);
+    }
+
+    /**
+     * @Route("/home", name="liste")
+     */
+    public function liste(SortieRepository $sortieRepository): Response
     {
         $sorties = $sortieRepository->affichageSortieAccueil();
         return $this->render('main/home.html.twig', [
