f423810818f9e46e2c1eed08212462938e19e021
XavierNormandiere <xnormandiere@gmail.com>
2021-10-12 12:11:04 +0200

    profil form

---
+++ src/Form/ProfileType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username', TextType::class, [
+                'label' => 'Pseudo'
+            ])
+            ->add('prenom', TextType::class, [
+                'label' => 'Prenom'
+            ])
+            ->add('nom', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('telephone', TextType::class, [
+                'label' => 'Telephone'
+            ])
+            ->add('email', TextType::class, [
+                'label' => 'Email'
+            ])
+            ->add('password', TextType::class, [
+                'label' => 'Mot de passe'
+            ])
+            ->add('campus', ChoiceType::class, [
+                'choices' => [
+                    'Nantes' => '3',
+                    'Rennes' => '2',
+                    'Niort' => '1',
+                    'Quimper' => '4'
+                ],
+                'multiple' => false
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
